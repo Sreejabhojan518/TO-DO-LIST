e60a0621d2e5200a390d9e3bbb686d66c08f2377
sreeja <sreejabhojan518@gmail.com>
2024-03-29 19:12:22 +0530

    made changes in css

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,18 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-var span = document.createElement("SPAN");
-var txt = document.createTextNode("\u00D7");
-span.className = "close";
-span.appendChild(txt);
-myNodelist[i].appendChild(span);
-}
-
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-}
+const inputBox = document.getElementById("myInput");
+const listContainer = document.getElementById("myUL");
+function newElement(){
+    if(inputBox.value === ''){
+        alert("Write something!")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00D7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
 }
 
 var list = document.querySelector('ul');
@@ -22,30 +20,11 @@ list.addEventListener('click', function(ev) {
 if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
 }
+else if(ev.target.tagName === 'SPAN'){
+    ev.target.parentElement.remove();
+}
 }, false);
 
-function newElement() {
-var li = document.createElement("li");
-var inputValue = document.getElementById("myInput").value;
-var t = document.createTextNode(inputValue);
-li.appendChild(t);
-if (inputValue === '') {
-    alert("You must write something!");
-} else {
-    document.getElementById("myUL").appendChild(li);
-}
-document.getElementById("myInput").value = "";
 
-var span = document.createElement("SPAN");
-var txt = document.createTextNode("\u00D7");
-span.className = "close";
-span.appendChild(txt);
-li.appendChild(span);
 
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-}
+
